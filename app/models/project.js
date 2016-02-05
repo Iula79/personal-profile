0f18b97478904c6169adc8fd96beb52b92863162
Giulia Lake <giulialake@Giulias-MacBook-Pro.local>
2016-02-04 23:26:23 -0500

    added links

--- app/models/project.js
+++ app/models/project.js
@@ -2,8 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     title: DS.attr(),
-  description: DS.attr(),
-  technologies: DS.attr(),
-  image: DS.attr(),
-  link: DS.attr()
+    description: DS.attr(),
+    technologies: DS.attr(),
+    image: DS.attr(),
+    link: DS.attr()
 });
